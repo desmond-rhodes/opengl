1a2314154daf1f8c3d02bea3a628ffe5130f6661
Desmond Rhodes <desmond.rhodes@outlook.com>
2021-02-21 00:27:15 +0700

    Use Vertex Array

--- main.cc
+++ main.cc
@@ -25,7 +25,7 @@ int main(int argc, char* argv[]) {
 	std::thread thread(renderer, window, &terminate);
 
 	auto poll_limit_last {std::chrono::steady_clock::now()};
-	std::chrono::microseconds poll_limit_time {33333}; // 30Hz
+	std::chrono::microseconds poll_limit_time {33333}; /* 30Hz */
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -44,34 +44,50 @@ void window_refresh_callback(GLFWwindow* window) { glfwGetFramebufferSize(window
 
 void renderer(GLFWwindow* window, bool const* terminate) {
 	glfwMakeContextCurrent(window);
-	int current_width {0};
-	int current_height {0};
+	GLsizei current_width {0};
+	GLsizei current_height {0};
 
 	auto fps_limit_last {std::chrono::steady_clock::now()};
-	std::chrono::microseconds fps_limit_time {41666}; // 24Hz
+	std::chrono::microseconds fps_limit_time {41666}; /* 24Hz */
+
+	GLdouble vertex[12] {
+		-1.0, -1.0,  0.0,
+		 1.0, -1.0,  0.0,
+		 1.0,  1.0,  0.0,
+		-1.0,  1.0,  0.0
+	};
+	GLfloat colour[12];
+	GLuint index[4] {0, 1, 2, 3};
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_COLOR_ARRAY);
+	glVertexPointer(3, GL_DOUBLE, 0, vertex);
+	glColorPointer(3, GL_FLOAT, 0, colour);
 
 	while (!*terminate) {
 		if (current_width != window_width || current_height != window_height) {
-			/* Note that this data reading is unsafe */
+			/* WARNING: Data reading is not thread safe! */
 			current_width = window_width;
 			current_height = window_height;
 
 			glViewport(0, 0, current_width, current_height);
+
+			GLdouble w {1};
+			GLdouble h {1};
+			if (current_width > current_height)
+				w = static_cast<GLdouble>(current_width) / current_height;
+			else
+				h = static_cast<GLdouble>(current_height) / current_width;
+
 			glMatrixMode(GL_PROJECTION);
 			glLoadIdentity();
-			glOrtho(current_width/-2.0, current_width/2.0, current_height/-2.0, current_height/2.0, -1.0, 1.0);
+			glOrtho(-w, w, -h, h, -1.0, 1.0);
 		}
 
+		random_colour(colour);
+
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		GLfloat c[12]; random_colour(c);
-		glBegin(GL_QUADS);
-			glColor3f(c[ 0], c[ 1], c[ 2]); glVertex2f( 100,  100);
-			glColor3f(c[ 3], c[ 4], c[ 5]); glVertex2f(-100,  100);
-			glColor3f(c[ 6], c[ 7], c[ 8]); glVertex2f(-100, -100);
-			glColor3f(c[ 9], c[10], c[11]); glVertex2f( 100, -100);
-		glEnd();
-
+		glDrawElements(GL_QUADS, 4, GL_UNSIGNED_INT, index);
 		glfwSwapBuffers(window);
 
 		std::this_thread::sleep_for(fps_limit_time - (std::chrono::steady_clock::now() - fps_limit_last));
